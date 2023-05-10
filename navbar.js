94f9dac38de59eab6d79d139ff7b82049f2a581b
albonico <juanalbo100@gmail.com>
2023-05-10 14:57:31 -0300

    cambios

--- navbar.js
+++ navbar.js
@@ -1,11 +1,19 @@
-document.getElementById("search-button").addEventListener("click", function() {
-    const searchText = document.getElementById("search-input").value.toLowerCase();
-    const contentText = document.querySelector(".content").textContent.toLowerCase();
-    
-    if (contentText.includes(searchText)) {
-      window.find(searchText);
-    } else {
-      alert("La palabra buscada no fue encontrada.");
-    }
-  });
-  
+const form = document.querySelector("form");
+const input = document.querySelector('input[type="text"]');
+const content = document.querySelector("body");
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const searchTerm = input.value.trim();
+  if (searchTerm.length) {
+    const regex = new RegExp(searchTerm, "gi");
+    const matches = content.innerHTML.match(regex);
+    if (matches && matches.length) {
+        content.innerHTML = content.innerHTML.replace(regex, '<span class="highlight">$&</span>');
+        const highlighted = document.querySelector('.highlight');
+        highlighted.scrollIntoView();
+      } else {
+        alert('No se encontraron coincidencias.');
+      }
+  }
+});
