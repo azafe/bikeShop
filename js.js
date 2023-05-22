2e5e59fd2561fa9ef9ef1ec0807f0aa538c1c87b
albonico <juanalbo100@gmail.com>
2023-05-22 16:58:06 -0300

    cambios finalizado

--- js.js
+++ js.js
@@ -7,13 +7,65 @@ form.addEventListener("submit", (event) => {
   const searchTerm = input.value.trim();
   if (searchTerm.length) {
     const regex = new RegExp(searchTerm, "gi");
-    const matches = content.innerHTML.match(regex);
-    if (matches && matches.length) {
-        content.innerHTML = content.innerHTML.replace(regex, '<span class="highlight">$&</span>');
-        const highlighted = document.querySelector('.highlight');
-        highlighted.scrollIntoView();
-      } else {
-        alert('No se encontraron coincidencias.');
+    const textNodes = getTextNodes(content);
+
+    textNodes.forEach((node) => {
+      const matches = node.nodeValue.match(regex);
+      if (matches && matches.length) {
+        const fragment = document.createDocumentFragment();
+        const parts = node.nodeValue.split(regex);
+
+        parts.forEach((part, index) => {
+          const span = document.createElement('span');
+          span.textContent = part;
+          fragment.appendChild(span);
+
+          if (index < parts.length - 1) {
+            const match = document.createElement('span');
+            match.textContent = matches[index];
+            match.classList.add('highlight');
+            fragment.appendChild(match);
+          }
+        });
+
+        node.parentNode.replaceChild(fragment, node);
       }
+    });
+
+    const highlighted = document.querySelector('.highlight');
+    if (highlighted) {
+      highlighted.scrollIntoView();
+    } else {
+      alert('No se encontraron coincidencias.');
+    }
   }
 });
+
+input.addEventListener("input", () => {
+  removeHighlights();
+});
+
+function getTextNodes(element) {
+  const textNodes = [];
+
+  function traverse(element) {
+    for (let node of element.childNodes) {
+      if (node.nodeType === Node.TEXT_NODE) {
+        textNodes.push(node);
+      } else if (node.nodeType === Node.ELEMENT_NODE) {
+        traverse(node);
+      }
+    }
+  }
+
+  traverse(element);
+  return textNodes;
+}
+
+function removeHighlights() {
+  const highlighted = document.querySelectorAll('.highlight');
+  highlighted.forEach((element) => {
+    const parent = element.parentNode;
+    parent.replaceChild(document.createTextNode(element.textContent), element);
+  });
+}
