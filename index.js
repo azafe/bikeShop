53fce144d3134bd9a1096929541976640cd29f7e
Fernando Zafe <fernandozafe@gmail.com>
2023-05-29 20:19:31 -0300

    Login aparece

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ let isAdmin = JSON.parse(localStorage.getItem("variable"));
 
 const admin = document.getElementById("admin");
 
+const panelAdmin = document.getElementById("panelAdmin");
+
+
 
 
 console.log("FUNCIONA EL INDEX");
@@ -11,6 +14,8 @@ console.log(isAdmin);
 
 if(isAdmin){
     admin.style.display = "block";
+    panelAdmin.style.display = "block";
 }else{
     admin.style.display = "none";
+    panelAdmin.style.display = "none";
 }
